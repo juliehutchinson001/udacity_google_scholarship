7d3cd26b79d91c035c0c81f949459c0e11a724dc
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-20 22:43:53 -0700

    chore: conditionals to check bank account

---
+++ conditionals_quiz/checking_balance.js
@@ -0,0 +1,23 @@
+/*
+ * Programming Quiz - Checking Your Balance (3-5)
+ */
+
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+var balance = 325.00;
+var checkBalance = true;
+var isActive = false;
+
+// your code goes here
+if(checkBalance === true) {
+    if(isActive === true && balance > 0) {
+        console.log("Your balance is $" + balance.toFixed(2) + ".");
+    } else if (isActive !== true) {
+        console.log("Your account is no longer active.");
+    } else if (balance === 0) {
+        console.log("Your account is empty.");
+    } else {
+        console.log("Your balance is negative. Please contact bank.");
+    }
+} else {
+    console.log("Thank you. Have a nice day!");
+}
