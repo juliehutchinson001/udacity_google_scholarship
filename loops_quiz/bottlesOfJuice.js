ff119355cd6714132941ab015c94095d4e13b6c9
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-27 22:07:03 -0700

    init: sample template to quiz solution

---
+++ loops_quiz/bottlesOfJuice.js
@@ -0,0 +1,19 @@
+/*
+ * Programming Quiz: 99 Bottles of Juice (4-2)
+ *
+ * Use the following `while` loop to write out the song "99 bottles of juice".
+ * Log the your lyrics to the console.
+ *
+ * Note
+ *   - Each line of the lyrics needs to be logged to the same line.
+ *   - The pluralization of the word "bottle" changes from "2 bottles" to "1 bottle" to "0 bottles".
+ */
+
+var num = 99;
+
+while (/* your stop condition goes here */) {
+    // check value of num
+    // print lyrics using num
+    // don't forget to check pluralization on the last line!
+    // decrement num
+}
