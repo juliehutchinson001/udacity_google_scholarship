7bc3fa2cf9683c2d1a58944e31509ce81359289b
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-27 21:12:08 -0700

    init: sample template to quiz solution

---
+++ loops_quiz/juliaJames.js
@@ -0,0 +1,11 @@
+/*
+ * Programming Quiz: JuliaJames (4-1)
+ */
+
+var x = 1;
+
+while (/* your stop condition goes here */) {
+    // check divisibility
+    // print Julia, James, or JuliaJames
+    // increment x
+}
