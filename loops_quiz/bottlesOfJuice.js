ce700aabf7ef0408a0a69804259058c72c2db5c9
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-27 22:18:59 -0700

    fix: change print statement for correct output

--- loops_quiz/bottlesOfJuice.js
+++ loops_quiz/bottlesOfJuice.js
@@ -18,9 +18,9 @@ while (num >= 1 /* your stop condition goes here */) {
         console.log(num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... " + (num - 1) + " bottles of juice on the wall!");
     } else if (num === 2) {
     // don't forget to check pluralization on the last line!
-        console.log(num + " bottles of juice on the wall! " + num + " bottle of juice! Take one down, pass it around... " + (num - 1) + " bottles of juice on the wall!");
+        console.log(num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... " + (num - 1) + " bottle of juice on the wall!");
     } else {
-        console.log(num + " bottle of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... " + (num - 1) + " bottles of juice on the wall!");
+        console.log(num + " bottle of juice on the wall! " + num + " bottle of juice! Take one down, pass it around... " + (num - 1) + " bottles of juice on the wall!");
     }
     // decrement num
     num --;
