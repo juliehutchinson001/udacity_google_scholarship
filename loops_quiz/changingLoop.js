0d61ee92280003f7e508abbecbcfea22cda955e6
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-29 16:50:33 -0700

    init: template to changing the loop quiz

---
+++ loops_quiz/changingLoop.js
@@ -0,0 +1,14 @@
+/*
+ * Programming Quiz: Changing the Loop (4-4)
+ */
+
+
+
+// rewrite the while loop as a for loop
+var x = 9;
+while (x >= 1) {
+    console.log("hello " + x);
+    x = x - 1;
+}
+
+// ------------------------------------
