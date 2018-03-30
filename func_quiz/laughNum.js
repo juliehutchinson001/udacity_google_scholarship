0b20e130f73152d17c81f24504e6f3e03c6e304b
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-29 20:20:58 -0700

    init: template of laugh it off a number of times quiz

---
+++ func_quiz/laughNum.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Laugh it Off 2 (5-2)
+ *
+ * Write a function called `laugh` with a parameter named `num` that 
+ * represents the number of "ha"s to return.
+ *
+ * Note:
+ *  - make sure your the final character is an exclamation mark ("!")
+ *  - make sure that your function produces the correct results when it 
+ *    is called multiple times
+ */
+
