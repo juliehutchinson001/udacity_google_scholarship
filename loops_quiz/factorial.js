76d1883a365602b59d530fe40e984b101cef3738
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-29 17:33:01 -0700

    init: template to solve factorial quiz

---
+++ loops_quiz/factorial.js
@@ -0,0 +1,15 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ * 
+ * Directions:
+ * Write a for loop that prints out the factorial of the number 12:
+ * 
+ * A factorial is calculated by multiplying a number by all the numbers below it. For instance, 3! or "3 factorial" is 3 * 2 * 1 = 6
+ * 
+ * 3! = 3 * 2 * 1 = 6 3!=3∗2∗1=6
+ * 4! = 4 * 3 * 2 * 1 = 24 4!=4∗3∗2∗1=24
+ * 5! = 5 * 4 * 3 * 2 * 1 = 120 5!=5∗4∗3∗2∗1=120
+ * 
+ */
+
+// your code goes here
