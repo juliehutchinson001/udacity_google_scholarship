4a43c164b87ca204a1af19a346b14e9a11a5f8d5
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-29 17:27:43 -0700

    init: template to solve fix the error quiz

---
+++ loops_quiz/fixError.js
@@ -0,0 +1,18 @@
+/*
+ * Programming Quiz: Fix the Error 1 (4-5)
+ * 
+ * 
+ * 
+ * 
+ * Directions:
+ * Here is a for loop that's supposed to print the numbers 5 through 9. Fix the error!
+ *
+ * for (x < 10; x++) {
+ *   console.log(x);
+ * }
+ */
+
+// fix the for loop
+for (x < 10; x++) {
+    console.log(x);
+}
