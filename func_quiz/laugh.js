16d9237a49dbb17da9a4fb33d5197a2ef352b53f
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-29 19:59:50 -0700

    feat: solution to laugh it off quizz

---
+++ func_quiz/laugh.js
@@ -0,0 +1,18 @@
+/*
+ * Programming Quiz: Laugh it Off 1 (5-1)
+ * 
+ * Directions:
+ * Declare a function called laugh() that returns "hahahahahahahahahaha!".
+ * Print the value returned from the laugh() function to the console.
+ * 
+ */
+
+// your code goes here
+
+function laugh() {
+    var astring = "hahahahahahahahahaha!";
+    return astring;
+}
+
+/* call the laugh function */
+console.log(laugh());
