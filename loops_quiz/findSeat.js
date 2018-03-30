fba6453ec040822103ae943904afea89a40951bb
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-29 19:21:51 -0700

    init: template to solve find my seat quiz

---
+++ loops_quiz/findSeat.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Find my Seat (4-8)
+ * 
+ * Write a nested for loop to print out all of the different seat combinations in the theater.
+ * The first row-seat combination should be 0-0 
+ * The last row-seat combination will be 25-99
+ * 
+ * Things to note: 
+ *  - the row and seat numbers start at 0, not 1
+ *  - the highest seat number is 99, not 100
+ */
+
+// Write your code here
