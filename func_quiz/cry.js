97e5882992c3d8c4788a2df1427cc652b51f6081
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-31 02:07:59 -0700

    init: template to solve cry quiz

---
+++ func_quiz/cry.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: Cry (5-5)
+ * Directions:
+ * Write a named function expression that stores the function in a variable called cry and returns "boohoo!". Don't forget to call the function using the variable name, not the function name:
+ * 
+ * cry();
+ * Returns: boohoo!
+ */
+
+// your code goes here
