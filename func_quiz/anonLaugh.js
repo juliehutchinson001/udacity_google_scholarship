735837718f9d7b30339977ce6323ec15370bc347
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-31 01:24:49 -0700

    init: template to solve laugh function quiz

---
+++ func_quiz/anonLaugh.js
@@ -0,0 +1,15 @@
+/*
+ * Programming Quiz: Laugh (5-4)
+ * 
+ * Directions:
+ * Write an anonymous function expression that stores a function in a variable called "laugh" and outputs the number of "ha"s that you pass in as an argument.
+ * 
+ * laugh(3);
+ * 
+ * >>> hahaha!
+ * 
+ */
+
+var laugh = /* finish the function expression */
+
+console.log(laugh(10));
