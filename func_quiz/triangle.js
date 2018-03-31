486268d7760c7feda291674deb20397b4d68d87c
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-30 22:47:07 -0700

    feat: solution to build a triangle quiz

--- func_quiz/triangle.js
+++ func_quiz/triangle.js
@@ -12,7 +12,15 @@ function makeLine(length) {
 }
 
 // your code goes here.  Make sure you call makeLine() in your own code.
+function buildTriangle(num) {
+    var piramid = "";
+    for (var aCounter = 1 ; aCounter <= num ; aCounter++) {
+        piramid += makeLine(aCounter);
+    }
+    return piramid;
+}
 
+console.log(buildTriangle(10));
 
 // test your code by uncommenting the following line
 //console.log(buildTriangle(10));
