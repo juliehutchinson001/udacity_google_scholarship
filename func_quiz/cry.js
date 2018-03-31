a8153ca8b9f0e78418226af903a4491785e43059
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-31 02:14:48 -0700

    feat: solution to a named function expression quiz

--- func_quiz/cry.js
+++ func_quiz/cry.js
@@ -1,10 +1,20 @@
 /*
  * Programming Quiz: Cry (5-5)
  * Directions:
- * Write a named function expression that stores the function in a variable called cry and returns "boohoo!". Don't forget to call the function using the variable name, not the function name:
+ * Write a named function expression that stores the function in a 
+ * variable called cry and returns "boohoo!". Don't forget to call the 
+ * function using the variable name, not the function name:
  * 
  * cry();
  * Returns: boohoo!
  */
 
 // your code goes here
+
+var cry = function test(boo) {
+    boo = "boohoo!";
+    
+    return boo;
+};
+
+cry();
