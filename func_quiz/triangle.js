c1fc563a440ba67f20955c2472db82e76aaa96e8
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-30 22:33:10 -0700

    init: definition of the triangle quiz

---
+++ func_quiz/triangle.js
@@ -0,0 +1,18 @@
+/*
+ * Programming Quiz: Build A Triangle (5-3)
+ */
+
+// creates a line of * for a given length
+function makeLine(length) {
+    var line = "";
+    for (var j = 1; j <= length; j++) {
+        line += "* ";
+    }
+    return line + "\n";
+}
+
+// your code goes here.  Make sure you call makeLine() in your own code.
+
+
+// test your code by uncommenting the following line
+//console.log(buildTriangle(10));
