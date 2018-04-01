b8b885f6797dab50ddc4806c0d9dd8060553ab83
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-31 23:28:00 -0700

    init: array quiz 3 template

---
+++ arrays_quiz/rightPrice.js
@@ -0,0 +1,18 @@
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ * 
+ * Directions:
+ * Starting with this array of prices, change the prices of the 1st, 3rd, 
+ * and 7th elements in the array.
+ * 
+ * var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+ * TIP: The 1st element of any array has an index of 0.
+ * 
+ * Afterwards, print out the prices array to the console.
+ * 
+ * 
+ */
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+// your code goes here
