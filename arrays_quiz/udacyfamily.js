f7bc44f4e795fee53c49c172cf21483e567e48cb
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-31 23:11:52 -0700

    init: array quiz template

---
+++ arrays_quiz/udacyfamily.js
@@ -0,0 +1,9 @@
+/*
+ * Programming Quiz: UdaciFamily (6-1)
+ * Directions:
+ * Create an array called udaciFamily and add "Julia", "James", and your 
+ * name to the array. Then, print the udaciFamily to the console using 
+ * console.log.
+ */
+
+// your code goes here
