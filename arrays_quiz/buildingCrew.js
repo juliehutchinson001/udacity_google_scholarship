a38f5871d7c7f3af561bda012595a3fb0445c92a
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-31 23:25:21 -0700

    feat: solution to building a crew quiz for arrays

--- arrays_quiz/buildingCrew.js
+++ arrays_quiz/buildingCrew.js
@@ -1,28 +1,35 @@
-
-
-
 /*
  * Programming Quiz: Building the Crew (6-2)
  * 
- * The space western TV drama Firefly premiered in the United States on September 20, 2002. Although the show only featured fourteen episodes and was canceled during its first season, it continues to remain popular today with a growing fan base. In the show, the captain Mal, played by Nathan Fillion, leads a crew of renegades on the spaceship Serenity.
+ * The space western TV drama Firefly premiered in the United States on 
+ * September 20, 2002. Although the show only featured fourteen episodes 
+ * and was canceled during its first season, it continues to remain popular 
+ * today with a growing fan base. In the show, the captain Mal, played by 
+ * Nathan Fillion, leads a crew of renegades on the spaceship Serenity.
  * 
  * Directions:
- * Create an array called crew to organize the Serenity’s crew and set it equal to the variables below . You don't need to type out the actual strings, just use the provided variables.
+ * Create an array called crew to organize the Serenity’s crew and set it 
+ * equal to the variables below . You don't need to type out the actual 
+ * strings, just use the provided variables.
  * 
- * var captain = "Mal";
- * var second = "Zoe";
- * var pilot = "Wash";
- * var companion = "Inara";
- * var mercenary = "Jayne";
- * var mechanic = "Kaylee";
+ * let captain = "Mal";
+ * let second = "Zoe";
+ * let pilot = "Wash";
+ * let companion = "Inara";
+ * let mercenary = "Jayne";
+ * let mechanic = "Kaylee";
  * Then, print the crew array to the console.
  */
 
-var captain = "Mal";
-var second = "Zoe";
-var pilot = "Wash";
-var companion = "Inara";
-var mercenary = "Jayne";
-var mechanic = "Kaylee";
+let captain = "Mal";
+let second = "Zoe";
+let pilot = "Wash";
+let companion = "Inara";
+let mercenary = "Jayne";
+let mechanic = "Kaylee";
 
 // your code goes here
+
+let crew = [captain, second, pilot, companion, mercenary, mechanic];
+
+console.log(crew);
