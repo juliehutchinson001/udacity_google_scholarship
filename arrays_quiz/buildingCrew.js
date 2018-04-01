776bd1db869e5c0bb73434c446386a4c733748d9
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-03-31 23:17:19 -0700

    init: array quiz 2 template

---
+++ arrays_quiz/buildingCrew.js
@@ -0,0 +1,28 @@
+
+
+
+/*
+ * Programming Quiz: Building the Crew (6-2)
+ * 
+ * The space western TV drama Firefly premiered in the United States on September 20, 2002. Although the show only featured fourteen episodes and was canceled during its first season, it continues to remain popular today with a growing fan base. In the show, the captain Mal, played by Nathan Fillion, leads a crew of renegades on the spaceship Serenity.
+ * 
+ * Directions:
+ * Create an array called crew to organize the Serenity’s crew and set it equal to the variables below . You don't need to type out the actual strings, just use the provided variables.
+ * 
+ * var captain = "Mal";
+ * var second = "Zoe";
+ * var pilot = "Wash";
+ * var companion = "Inara";
+ * var mercenary = "Jayne";
+ * var mechanic = "Kaylee";
+ * Then, print the crew array to the console.
+ */
+
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+
+// your code goes here
