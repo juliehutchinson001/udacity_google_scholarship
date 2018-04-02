b0a6519f33420ecdd39909b4e4392879c1a1456d
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-01 23:52:40 -0700

    init: template to solve rainbow Colors quiz

---
+++ arrays_quiz/rainbowColors.js
@@ -0,0 +1,22 @@
+/*
+ * Programming Quiz: Colors of the Rainbow (6-4)
+ * 
+ * Directions:
+ * James was creating an array with the colors of the rainbow, and he forgot some colors. The standard rainbow colors are usually listed in this order:
+ * 
+ * var rainbow = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"];
+ * but James had this:
+ * 
+ * var rainbow = ["Red", "Orange", "Blackberry", "Blue"];
+ * Using only the splice() method, insert the missing colors into the array, and remove the color "Blackberry" by following these steps:
+ * 
+ * Remove "Blackberry"
+ * Add "Yellow" and "Green"
+ * Add "Purple"
+ * 
+ * 
+ */
+
+var rainbow = ["Red", "Orange", "Blackberry", "Blue"];
+
+// your code goes here
