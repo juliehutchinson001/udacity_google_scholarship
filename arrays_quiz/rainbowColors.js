d3687f09643e7a7d48c6794f6f414755918c6df3
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-02 01:34:17 -0700

    refactor: cut the splice and push lines into a single one using splice

--- arrays_quiz/rainbowColors.js
+++ arrays_quiz/rainbowColors.js
@@ -22,8 +22,7 @@
 let rainbow = ["Red", "Orange", "Blackberry", "Blue"];
 
 // your code goes here
-rainbow.splice(2,1);
-rainbow.push("Yellow", "Green");
+rainbow.splice(2,1, "Yellow", "Green");
 rainbow.push("Purple");
 
 console.log(rainbow);
