60d7d08d4cbbf5e748b98c6997533312e14190a1
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-02 01:48:34 -0700

    init: template to solve rainbow Colors quiz

---
+++ arrays_quiz/harryPotter.js
@@ -0,0 +1,26 @@
+/*
+ * Programming Quiz: Quidditch Cup (6-5)
+ * 
+ * In the Harry Potter novels, children attending the Hogwarts 
+ * School of Witchcraft and Wizardry belong to one of four houses: 
+ * Gryffindor, Hufflepuff, Ravenclaw, or Slytherin. Each year, the 
+ * houses assemble a Quidditch team of seven players to compete for 
+ * the coveted Quidditch Cup.
+ * 
+ * Consider the following array.
+ * 
+ * var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", 
+ *             "Alicia Spinnet", "George Weasley", "Fred Weasley", 
+ *             "Harry Potter"];
+ * 
+ * Directions:
+ * Create a function called hasEnoughPlayers() that takes the team 
+ * array as an argument and returns true or false depending on if 
+ * the array has at least seven players.
+ * 
+ */
+
+// your code goes here
+
+var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+console.log(hasEnoughPlayers(team));
