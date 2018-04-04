e4877a6c979fa3c84ea0087e1a4ed1ba81b14d49
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-03 20:09:31 -0700

    init: setup template to solve join the crew array quiz

---
+++ arrays_quiz/joinTheCrew.js
@@ -0,0 +1,40 @@
+
+/*
+ * Programming Quiz: Joining the Crew (6-6)
+ * 
+ * Directions:
+ * In an earlier exercise, you created a crew array to represent Mal’s crew 
+ * from the hit show Firefly.
+ * 
+ * let captain = "Mal";
+ * let second = "Zoe";
+ * let pilot = "Wash";
+ * let companion = "Inara";
+ * let mercenary = "Jayne";
+ * let mechanic = "Kaylee";
+ * 
+ * let crew = [captain, second, pilot, companion, mercenary, mechanic];
+ * Later in the show, Mal takes on three new crew members named "Simon", 
+ * "River", and "Book". Use the push() method to add the three new crew 
+ * members to the crew array.
+ * 
+ * let doctor = "Simon";
+ * let sister = "River";
+ * let shepherd = "Book";
+ * 
+ */
+
+let captain = "Mal";
+let second = "Zoe";
+let pilot = "Wash";
+let companion = "Inara";
+let mercenary = "Jayne";
+let mechanic = "Kaylee";
+
+let crew = [captain, second, pilot, companion, mercenary, mechanic];
+
+let doctor = "Simon";
+let sister = "River";
+let shepherd = "Book";
+
+// your code goes here
