76fa454321f6318fcbc7b93a2cc18b7bd3728d2a
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-03 20:04:52 -0700

    feat: solution to Quiddich quiz

--- arrays_quiz/harryPotter.js
+++ arrays_quiz/harryPotter.js
@@ -22,5 +22,14 @@
 
 // your code goes here
 
-var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+
+function hasEnoughPlayers(teamArr) {
+    let verify = (teamArr.length >= 7) ? true : false;
+
+    return verify;
+}
+
+
+let team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+
 console.log(hasEnoughPlayers(team));
