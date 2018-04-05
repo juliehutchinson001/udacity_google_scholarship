decfc15a8895fcee995fd8e90682cce77664f0c8
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-05 01:05:40 -0700

    feat: solution of array quiz I got bills

--- arrays_quiz/gotBills.js
+++ arrays_quiz/gotBills.js
@@ -5,7 +5,7 @@
  * Use the map() method to take the array of bill amounts shown below, and create a second array of 
  * numbers called totals that shows the bill amounts with a 15% tip added.
  * 
- * var bills = [50.23, 19.12, 34.01, 100.11, 12.15, 9.90, 29.11, 12.99, 10.00, 99.22, 102.20, 
+ * let bills = [50.23, 19.12, 34.01, 100.11, 12.15, 9.90, 29.11, 12.99, 10.00, 99.22, 102.20, 
  *              100.10, 6.77, 2.22];
  * 
  * Print out the new totals array using console.log.
@@ -31,7 +31,16 @@
  *  - each number must have an accuracy of two decimal points
  */
 
-var bills = [50.23, 19.12, 34.01,
+let bills = [50.23, 19.12, 34.01,
     100.11, 12.15, 9.90, 29.11, 12.99,
     10.00, 99.22, 102.20, 100.10, 6.77, 2.22
 ];
+
+let tippedBills = bills.map(function(amountBilled) {
+    let tipPercent = amountBilled*0.15;
+    let totalTipped = amountBilled + tipPercent;
+
+    return Number(totalTipped.toFixed(2));
+});
+
+console.log(tippedBills);
