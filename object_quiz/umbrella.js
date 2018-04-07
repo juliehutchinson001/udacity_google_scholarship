c274544b56c4ecead9c4097cd480e09d7c201eb5
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-06 21:31:04 -0700

    init: template quiz for umbrella object

---
+++ object_quiz/umbrella.js
@@ -0,0 +1,31 @@
+/*
+ * Programming Quiz: Umbrella (7-1)
+ * 
+ * Directions:
+ * Using the umbrella example from the previous video, see if you can follow the 
+ * example open() method and create the close() method. It's alright if you have 
+ * trouble at first! We'll go into more detail later in this lesson.
+ * 
+ * TIP: Remember to put all of your object's properties and methods inside curly 
+ * braces: var myObject = { greeting: "hello", name: "Julia" };. Also, remember that 
+ * an object is just another data type. Just like how you would put a semicolon after 
+ * a string variable declaration var myString = "hello";, don't forget to put a 
+ * semi-colon at the end of your object's declaration.
+ * 
+ * 
+ */
+
+var umbrella = {
+    color: "pink",
+    isOpen: true,
+    open: function() {
+        if (umbrella.isOpen === true) {
+            return "The umbrella is already opened!";
+        } else {
+            umbrella.isOpen = true;
+            return "Julia opens the umbrella!";
+        }
+    },
+    // your code goes here
+    
+};
