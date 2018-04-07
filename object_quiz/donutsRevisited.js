04519e4d92efa8c77337b8b7e23799c97ba89b05
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-07 03:10:19 -0700

    init: template code of Donuts Revisited object quiz

---
+++ object_quiz/donutsRevisited.js
@@ -0,0 +1,29 @@
+/*
+ * Programming Quiz: Donuts Revisited (7-6)
+ * 
+ * Here is an array of donut objects.
+ * 
+ * let donuts = [
+ *   { type: "Jelly", cost: 1.22 },
+ *   { type: "Chocolate", cost: 2.45 },
+ *   { type: "Cider", cost: 1.59 },
+ *   { type: "Boston Cream", cost: 5.99 }
+ * ];
+ * Directions:
+ * Use the forEach() method to loop over the array and print out the following donut summaries using console.log.
+ * 
+ * Jelly donuts cost $1.22 each
+ * Chocolate donuts cost $2.45 each
+ * Cider donuts cost $1.59 each
+ * Boston Cream donuts cost $5.99 each
+ * 
+ */
+
+let donuts = [
+    { type: "Jelly", cost: 1.22 },
+    { type: "Chocolate", cost: 2.45 },
+    { type: "Cider", cost: 1.59 },
+    { type: "Boston Cream", cost: 5.99 }
+];
+
+// your code goes here
