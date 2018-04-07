66e7fe80993c128dd24e588e18ef0fd610686d69
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-06 21:32:20 -0700

    feat: solution for umbrella's close's method

--- object_quiz/umbrella.js
+++ object_quiz/umbrella.js
@@ -27,5 +27,12 @@ var umbrella = {
         }
     },
     // your code goes here
-    
+    close: function() {
+        if (umbrella.isOpen === false) {
+            return "The umbrella is already closed!";
+        } else {
+            umbrella.isOpen = false;
+            return "Julia closes the umbrella!";
+        }
+    }
 };
