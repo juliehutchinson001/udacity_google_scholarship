7c77d163bcf42b2fc1f3f1341f03bc6200eb9b0f
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-07 12:36:24 -0700

    init: template of javascript file to solve quiz about tag selectors

---
+++ DOM_jQuery_basics/select_by_tags/app.js
@@ -0,0 +1,8 @@
+/*
+For this quiz, use a jQuery tag selector to grab all of the <li>s on the page!
+*/
+
+// Start with this variable! (don't delete it!)
+var listElements;
+
+listElements = // your code goes here!
