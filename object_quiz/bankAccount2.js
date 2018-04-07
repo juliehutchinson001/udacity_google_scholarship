04f2723f11bd2e3c69a167470842fa378b7b04ea
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-07 02:46:39 -0700

    init: template code of bank account part 2 object quiz

---
+++ object_quiz/bankAccount2.js
@@ -0,0 +1,28 @@
+/*
+ * Quiz: Bank Accounts 2 (7-4)
+ * 
+ * Directions:
+ * Using the object from the previous quiz, answer the following quiz question:
+ * 
+ * var savingsAccount = {
+ *   balance: 1000,
+ *   interestRatePercent: 1,
+ *   deposit: function addMoney(amount) {
+ *     if (amount > 0) {
+ *       savingsAccount.balance += amount;
+ *     }
+ *   },
+ *   withdraw: function removeMoney(amount) {
+ *     var verifyBalance = savingsAccount.balance - amount;
+ *     if (amount > 0 && verifyBalance >= 0) {
+ *       savingsAccount.balance -= amount;
+ *     }
+ *   }
+ * };
+ * 
+ * Your Code:
+ * QUIZ QUESTION
+ * Which of the following are valid ways to access properties and call 
+ * methods from the savingsAccount object?
+ * 
+ */
