6c858b8b8db65f483f90efb48af91ef934f91e86
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-07 02:10:42 -0700

    init: template code of menu items object quiz

---
+++ object_quiz/menuItems.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Menu Items (7-2)
+ * 
+ * Directions:
+ * Create a breakfast object to represent the following menu item:
+ * 
+ * The Lumberjack - $9.95
+ * eggs, sausage, toast, hashbrowns, pancakes
+ * The object should contain properties for the name, price, and ingredients.
+ * 
+ */
+
+// your code goes here
