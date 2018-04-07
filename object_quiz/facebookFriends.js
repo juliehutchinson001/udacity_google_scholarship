f0fb52751156a64df781a82ce1df48b842f87c25
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-07 02:52:53 -0700

    init: template code of Facebook Friends object quiz

---
+++ object_quiz/facebookFriends.js
@@ -0,0 +1,19 @@
+/*
+ * Programming Quiz: Facebook Friends (7-5)
+ * 
+ * Directions:
+ * Create an object called facebookProfile. The object should have 3 properties:
+ * 
+ * your name
+ * the number of friends you have, and
+ * an array of messages you've posted (as strings)
+ * The object should also have 4 methods:
+ * 
+ * postMessage(message) - adds a new message string to the array
+ * deleteMessage(index) - removes the message corresponding to the index provided
+ * addFriend() - increases the friend count by 1
+ * removeFriend() - decreases the friend count by 1
+ * 
+ */
+ 
+ // your code goes here
