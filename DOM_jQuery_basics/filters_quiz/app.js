fd942bf31451878d65195a325512383c088e9209
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-07 21:13:06 -0700

    init: template of javascript file to solve filter quiz

---
+++ DOM_jQuery_basics/filters_quiz/app.js
@@ -0,0 +1,21 @@
+/*
+For this quiz, use articleList and DOM navigation methods to collect articleList's
+sibling <h1> (let h1), children (let kids), and parent <div>s (var parents).
+
+You must use articleList to navigate to the element(s)!
+*/
+
+// Start with these variable!
+let articleList, h1, kids, parents;
+
+articleList = $('.article-list');
+
+
+// Your code goes here!
+h1 = 
+
+// Your code goes here!
+kids = 
+
+// Your code goes here!
+parents = 
