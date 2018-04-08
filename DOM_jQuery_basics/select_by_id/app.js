38796d8e6316804bd0af05cd0e3c146fd35c61b9
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-07 19:48:27 -0700

    init: template of javascript file to solve quiz about id selector

---
+++ DOM_jQuery_basics/select_by_id/app.js
@@ -0,0 +1,10 @@
+/*
+For this quiz, use a jQuery class selector to grab all the element with 
+id 'nav' on the page!
+*/
+
+// don't change this variable!
+let nav;
+
+// your code goes here!
+nav = 
