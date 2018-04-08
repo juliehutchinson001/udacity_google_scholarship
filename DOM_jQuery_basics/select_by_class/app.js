5cad7268f172c764f6d2a94686dc745f2c8e6154
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-07 19:29:06 -0700

    init: template of html file to solve quiz about class selectors

---
+++ DOM_jQuery_basics/select_by_class/app.js
@@ -0,0 +1,10 @@
+/*
+For this quiz, use a jQuery class selector to grab all of the 
+elements of class 'article-item' on the page!
+*/
+
+// don't change this variable!
+let articleItems;
+
+// your code goes here!
+articleItems = 
