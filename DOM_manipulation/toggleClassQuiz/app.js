49908bfb71489e6f99c3d252a85e73f034c208a4
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-08 13:26:56 -0700

    feat: solution of toggleClass() quiz1

--- DOM_manipulation/toggleClassQuiz/app.js
+++ DOM_manipulation/toggleClassQuiz/app.js
@@ -1,9 +1,19 @@
 /*
-For this quiz, use a jQuery class selector and featuredArticle variable to toggle the 'featured' class!
+For this quiz, use a jQuery class selector and featuredArticle 
+variable to toggle the 'featured' class!
 */
 
 // don't change this variable!
 let featuredArticle;
 
 // your code starts here!
-featuredArticle = 
+featuredArticle = $('li').toggleClass('featured'); //my soln
+
+// -------------------------------------------------- //
+
+// don't change this variable!
+let featuredArticle;
+
+featuredArticle = $('.featured') //udacity's soln
+
+featuredArticle.toggleClass('featured');
