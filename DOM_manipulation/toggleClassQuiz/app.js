62f07cf050a6ea63404e7767066396ae909391c6
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-08 13:12:32 -0700

    init: template code in javascript file to solve toggleClass() quiz

---
+++ DOM_manipulation/toggleClassQuiz/app.js
@@ -0,0 +1,9 @@
+/*
+For this quiz, use a jQuery class selector and featuredArticle variable to toggle the 'featured' class!
+*/
+
+// don't change this variable!
+let featuredArticle;
+
+// your code starts here!
+featuredArticle = 
