2ebfd8ff4af5ea0299d5774575cbf67b951e67d9
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-08 18:01:14 -0700

    init: template code in javascript file to solve toggling Classes quiz2

---
+++ DOM_manipulation/togglingClassesQuiz/app.js
@@ -0,0 +1,11 @@
+/*
+For this quiz, remove the class 'featured' from Article #2 
+and add it to Article #3!
+
+You must use jQuery's toggleClass method!
+*/
+
+// don't change these variable!
+let article2, article3;
+
+// your code goes here!
