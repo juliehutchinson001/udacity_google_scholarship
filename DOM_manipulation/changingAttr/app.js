563ac2ce86200474af9720cfbc2c749296039619
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-09 19:57:57 -0700

    init: template code of javascript file to solve adding an attr quiz3

---
+++ DOM_manipulation/changingAttr/app.js
@@ -0,0 +1,11 @@
+/*
+For this quiz, set the href of the <a> in the first nav item to "#1".
+
+You must use jQuery's attr() method!
+*/
+
+// Start with this variable!
+let navList;
+
+// your code goes here!
+navList = 
