5f00c9153da3b1be0364378f7dec572499624997
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-09 23:27:07 -0700

    feat: udacity's soln to adding an attr()

--- DOM_manipulation/changingAttr/app.js
+++ DOM_manipulation/changingAttr/app.js
@@ -27,3 +27,15 @@ let navList;
 // your code goes here!
 navList = $('a');
 navList.attr("href", "#1");
+
+/* ---------------------------------------------------- */ 
+// Start with this variable!
+let navList;
+let firstItem;
+let link;
+
+// UDACITY's code goes here!
+navList = $('.nav-list');
+firstItem = navList.children().first();
+link = firstItem.find('a');
+link.attr("href", "#1");
