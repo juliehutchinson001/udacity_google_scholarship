43295e931171c25c7eb15e2cf616e0c7e8a6a083
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-10 10:10:12 -0700

    init: template code javascript file to solve changing css quiz4

---
+++ DOM_manipulation/modifyCSS/app.js
@@ -0,0 +1,12 @@
+/*
+ * For this quiz, change the font-size of all the 
+ * article-items to 20px!
+ * 
+ * You must use jQuery's css() method!
+ */
+
+// Start with this variable!
+var articleItems;
+
+// your code goes here!
+articleItems = 
