3a9f0c8469f8289bc85bf91a19c6ad7319865f33
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-10 20:44:58 -0700

    init: template code of Javascript file to solve collecting values quiz5

---
+++ DOM_manipulation/collectValues/app.js
@@ -0,0 +1,16 @@
+/*
+ * For this quiz, use jQuery's val method to make live 
+ * changes to the 'Cool Articles' <h1>!
+ * 
+ * The starter code below creates an event listener that 
+ * will run any time the input changes.
+ * For more on events, check the instructor notes.
+ */
+
+
+$('#input').on('change', function() {
+    var val;
+    
+    // Your code goes here!
+    val = 
+});
