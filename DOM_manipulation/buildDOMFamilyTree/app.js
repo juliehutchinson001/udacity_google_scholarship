3976b8931d152a5588f09b61df3b6d329a6fac4a
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-10 21:45:34 -0700

    feat: solution to build a family tree quiz7

--- DOM_manipulation/buildDOMFamilyTree/app.js
+++ DOM_manipulation/buildDOMFamilyTree/app.js
@@ -9,3 +9,16 @@
  */
 
 // Your code goes here!
+let family1 = $('#family1');
+let family2 = $('<div id="family2"><h1>Family 2</h1></div>');
+let bruce = $('<div id="bruce"><h2>Bruce</h2></div>');
+let madison = $('<div id="madison"><h3>Madison</h3></div>');
+let hunter = $('<div id="hunter"><h3>Hunter</h3></div>');
+
+family2.insertAfter(family1); //root of family 2 or sibling of family 1
+
+family2.append(bruce); // Bruce is the immediate child of family 2
+
+bruce.append(madison); // Madison is the first child of Bruce
+
+bruce.append(hunter); // Hunter is the second child of Bruce
