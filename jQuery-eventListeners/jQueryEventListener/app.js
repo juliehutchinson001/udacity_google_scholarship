ccae51d7dddd21e5bd5afcea8d347c76ca883f36
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-11 00:03:23 -0700

    init: template code of Javascript file to solve jQuery event listener quiz1

---
+++ jQuery-eventListeners/jQueryEventListener/app.js
@@ -0,0 +1,9 @@
+/*
+For this quiz, use jQuery to set up an event listener. Your event listener must:
+    1. listen to the #my-button element
+    2. listen for a `click` event
+    3. perform the following actions when the button is clicked:
+        a. remove the #my-button element from the DOM
+        b. add the `success` class to the body
+*/
+
