2a9bdc337387a0cfff0d79f1338c306f2a4956dd
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-10 22:57:10 -0700

    init: template code of Javascript file to solve iterating through each element quiz8

---
+++ DOM_manipulation/iteratingEachQuiz/app.js
@@ -0,0 +1,16 @@
+/*
+ * For this quiz, use jQuery's each() method to iterate 
+ * through the <p>s, calculate the length of each one, 
+ * and add each length to the end of each <p>.
+ * 
+ * Also, make sure you don't change the text inside each 
+ * <p> except to add the length, otherwise your length 
+ * numbers won't be correct!
+ */
+
+// Your code goes here!
+
+
+
+
+
