5fdfdb853c531e953bfaf8c95054855b7bae8417
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-10 21:07:27 -0700

    feat: solution to remove element quiz6

---
+++ DOM_manipulation/removeDOMel/app.js
@@ -0,0 +1,12 @@
+/*
+ * For this quiz, remove the <ul> from the first article 
+ * item!
+ * 
+ * You must use jQuery's remove() method.
+ */
+
+// Start with this variable!
+let articleItems;
+
+// your code goes here!
+articleItems = $('.article-list').find('ul').remove();
