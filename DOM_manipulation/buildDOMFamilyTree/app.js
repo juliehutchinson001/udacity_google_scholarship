b10465255cd65513ce80517b129b6d449df83244
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-10 21:26:12 -0700

    init: template code of Javascript file to solve build a family tree quiz7

---
+++ DOM_manipulation/buildDOMFamilyTree/app.js
@@ -0,0 +1,11 @@
+/*
+ * For this quiz, you'll need to add to the DOM tree that 
+ * already exists.
+ * 
+ * '#family2' should be a sibling of and come after 
+ * '#family1'. '#bruce' should be the only immediate child
+ * of '#family2'. '#bruce' should have two <div>s as 
+ * children, '#madison' and '#hunter'.
+ */
+
+// Your code goes here!
