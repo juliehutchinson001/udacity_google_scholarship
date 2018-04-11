21c11eb208b6cb04b4d7b84f17a9b86931483119
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-10 21:11:41 -0700

    feat: UDACITY's solution to remove element quiz6

--- DOM_manipulation/removeDOMel/app.js
+++ DOM_manipulation/removeDOMel/app.js
@@ -10,3 +10,17 @@ let articleItems;
 
 // your code goes here!
 articleItems = $('.article-list').find('ul').remove();
+
+
+/* ---------------------------------------------------- */
+
+// Start with this variable!
+let articleItems;
+let ul;
+
+//UDACITY's code goes here!
+articleItems = $('.article-list')
+
+ul = articleItems.find('ul');
+
+ul.remove(); //a selector can be passed to remove() method
