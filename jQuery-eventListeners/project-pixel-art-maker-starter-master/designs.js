e7c2dbdafb9f4a0d594f396d3b9afc6618aa785a
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-12 23:29:51 -0700

    feat: jQuery Js file of pixel project

--- jQuery-eventListeners/project-pixel-art-maker-starter-master/designs.js
+++ jQuery-eventListeners/project-pixel-art-maker-starter-master/designs.js
@@ -1,10 +1,35 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
+$(document).ready(function() {
+    $('#sizePicker').submit(function() {
+        clearGrid();
+        makeGrid();
+        event.preventDefault();
+    });
+});
 
 function makeGrid() {
+    let makeColumns = $('#inputHeight').val();
+	let makeRows = $('#inputWidth').val();
+	let theTable = '<tbody>';
+	for (x = 1; x <= makeColumns; x++) {
+		theTable += '<tr>';
+		for (y = 1; y <= makeRows; y++) {
+			theTable += '<td>&nbsp;</td>';
+		}
+		theTable += '</tr></tbody>';	
+	}
+	$('#pixelCanvas').html(theTable);
+};
 
-// Your code goes here!
-
+function clearGrid() {
+    $('#pixelCanvas').children().remove();
 }
+
+$('#pixelCanvas').delegate('td', 'click', function(e) {
+	let yourColor = $('#colorPicker').val();
+	if (e.shiftKey) {
+		$(this).css('background', 'none');
+	} else {
+		$(this).css('background-color', yourColor);
+	};
+});
+
