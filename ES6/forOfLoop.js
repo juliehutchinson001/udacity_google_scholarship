7d0c48dd8a0f61fdb16bb644d130ff5cce2eeee1
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-07-10 11:21:45 -0700

    init: instructions to solve for...of loop quiz 2

---
+++ ES6/forOfLoop.js
@@ -0,0 +1,22 @@
+/* Directions:
+ * Write a for...of loop that:
+ *
+ * loops through each day in the days array
+ * capitalizes the first letter of the day
+ * and prints the day out to the console
+ * Your code should log the following to the console:
+ *
+ * Sunday
+ * Monday
+ * Tuesday
+ * Wednesday
+ * Thursday
+ * Friday
+ * Saturday
+ *
+ */
+
+const days = ['sunday', 'monday', 'tuesday', 'wednesday',
+            'thursday', 'friday', 'saturday'];
+
+// your code goes here
