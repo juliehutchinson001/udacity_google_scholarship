97e168c1bdbfb40f4c2c4988ca1ef66803f8288e
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-07-10 22:31:27 -0700

    init: instructions to solve the spread operator quiz 3

---
+++ ES6/spread_op.js
@@ -0,0 +1,11 @@
+/*
+ * Instructions: Use the spread operator to combine the
+ * `fruits` and `vegetables` arrays into the `produce` array.
+ */
+
+const fruits = ["apples", "bananas", "pears"];
+const vegetables = ["corn", "potatoes", "carrots"];
+
+const produce = [];
+
+console.log(produce);
