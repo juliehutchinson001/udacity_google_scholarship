e88997207bb1f4732c29f08e8148382e8d69ccb4
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-07-10 22:32:22 -0700

    feat: solution of the spread operator quiz

--- ES6/spread_op.js
+++ ES6/spread_op.js
@@ -6,6 +6,6 @@
 const fruits = ["apples", "bananas", "pears"];
 const vegetables = ["corn", "potatoes", "carrots"];
 
-const produce = [];
+const produce = [...fruits, ...vegetables];
 
 console.log(produce);
