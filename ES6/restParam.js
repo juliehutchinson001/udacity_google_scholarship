d25b960ed26ea8802052ed38f25b13de76d7ba29
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-07-10 22:46:01 -0700

    init: instructions to solve the rest parameter quiz 4

---
+++ ES6/restParam.js
@@ -0,0 +1,28 @@
+/*
+ * Directions:
+ * Use the rest parameter to create an average() function
+ * that calculates the average of an unlimited amount of
+ * numbers.
+ *
+ * TIP: Make sure to test your code with different values.
+ * For example,
+ *
+ * average(2, 6) should return 4
+ * average(2, 3, 3, 5, 7, 10) should return 5
+ * average(7, 1432, 12, 13, 100) should return 312.8
+ * average() should return 0
+ *
+ * Programming Quiz: Using the Rest Parameter (1-5)
+ */
+
+// your code goes here
+
+function average() {
+    
+}
+
+console.log(average(2, 6));
+console.log(average(2, 3, 3, 5, 7, 10));
+console.log(average(7, 1432, 12, 13, 100));
+console.log(average());
+
