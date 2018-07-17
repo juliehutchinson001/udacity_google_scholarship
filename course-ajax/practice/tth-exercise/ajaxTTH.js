0c6874db1950e3a3e6c8e59a1dc868aab3681d26
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-07-17 14:42:33 -0700

    feat: full xml request example

--- course-ajax/practice/tth-exercise/ajaxTTH.js
+++ course-ajax/practice/tth-exercise/ajaxTTH.js
@@ -1,4 +1,19 @@
 /*
  * Add the code to open the AJAX request using the
  * GET method and pointing to the 'footer.html' file.
+ * Then, send the ajax request.
 */
+
+let request = new XMLHttpRequest();
+
+request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+        let content = document.getElementById("footer");
+
+        content.innerHTML = request.responseText;
+    }
+}
+
+request.open('GET', "footer.html");
+request.send();
+
